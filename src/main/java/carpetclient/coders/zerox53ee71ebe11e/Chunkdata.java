a776886c9d7ad830f8c5040c069eabc6a9f489d5
0x53ee71ebe11e <git@pony.wtf>
2018-10-12 22:45:43 +0100

    fixed unloading state color

--- src/main/java/carpetclient/coders/zerox53ee71ebe11e/Chunkdata.java
+++ src/main/java/carpetclient/coders/zerox53ee71ebe11e/Chunkdata.java
@@ -61,7 +61,7 @@ public class Chunkdata implements Serializable {
         }
     }
 
-    private static final int STATE_WAS_UNLOAD_QUEUED = 0xffEAEA00;
+    private static final int STATE_WAS_UNLOAD_QUEUED = 0xff999900;
     private static final int STATE_WAS_PLAYER_LOADED = 0xff007000;
     private static final int STATE_LOADED = 0xff2C6798;
     private static final int STATE_PAST_LOADED = 0xff222222;
