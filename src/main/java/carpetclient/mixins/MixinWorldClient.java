bcff5a8551b259102615300316a6d6e67240f87f
Unknown <sahandhesar@gmail.com>
2018-12-05 12:03:47 +0100

    Fix for piston blinking

---
+++ src/main/java/carpetclient/mixins/MixinWorldClient.java
@@ -0,0 +1,21 @@
+package carpetclient.mixins;
+
+import carpetclient.Config;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(WorldClient.class)
+public abstract class MixinWorldClient {
+    @Inject(method = "invalidateRegionAndSetBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/WorldClient;invalidateBlockReceiveRegion(IIIIII)V"), cancellable = true)
+    private void fixPistonBlinking(BlockPos pos, IBlockState state, CallbackInfoReturnable<Boolean> cir) {
+        if(Config.clipThroughPistons && state.getBlock() == Blocks.PISTON_EXTENSION) {
+            cir.setReturnValue(true);
+        }
+    }
+}
