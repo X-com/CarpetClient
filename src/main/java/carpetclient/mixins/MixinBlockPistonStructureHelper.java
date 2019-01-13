aaafca4d7b7a24d3d37d2ee150c9e7402dcdb0c5
Unknown <sahandhesar@gmail.com>
2019-01-13 13:30:33 +0100

    Fixed broken mixin

--- src/main/java/carpetclient/mixins/MixinBlockPistonStructureHelper.java
+++ src/main/java/carpetclient/mixins/MixinBlockPistonStructureHelper.java
@@ -2,6 +2,8 @@ package carpetclient.mixins;
 
 import carpetclient.Config;
 import net.minecraft.block.state.BlockPistonStructureHelper;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
@@ -12,8 +14,8 @@ public class MixinBlockPistonStructureHelper {
     /*
     Edits the push limit of pistons to allow visually better
      */
-    @ModifyConstant(method = "addBlockLine", constant = @Constant(intValue = 12), remap = false)
-    private static int pushLimit(int pushlimit) {
+    @ModifyConstant(method = "addBlockLine", constant = @Constant(intValue = 12), expect = 3)
+    private static int pushLimit(int orig) {
         return Config.pushLimit;
     }
 }
