9a94f371f7d4b8f11fe10c9c210933db4e9836e2
Unknown <sahandhesar@gmail.com>
2019-01-17 11:53:38 +0100

    More piston helper refactors

--- src/main/java/carpetclient/coders/EDDxample/PistonHelper.java
+++ src/main/java/carpetclient/coders/EDDxample/PistonHelper.java
@@ -18,21 +18,21 @@ import net.minecraft.world.World;
  * A class to help visualize piston update order. Code provided by EDDxample.
  */
 public class PistonHelper {
-    /* Settings */
-    public static final String nope = "\u00a71\u00a74NOPE", gold = "\u00a76", red = "\u00a74", green = "\u00a72Blocks", pushe = "\u00a76Pushes", pull = "\u00a76Pull";
+
+    private static final String gold = "\u00a76", red = "\u00a74===", green = "\u00a72Blocks", pushe = "\u00a76Pushes", pull = "\u00a76Pull";
 
     public static boolean validState, activated, extending;
     public static BlockPos pistonPos;
-    public static BlockPos[] tobreak, tomove;
+    private static BlockPos[] tobreak, tomove;
 
-    public static void set(BlockPos posIn, BlockPos[] btm, BlockPos[] btb, boolean isValid, boolean _extending) {
-        pistonPos = posIn;
-        tomove = btm;
-        tobreak = btb;
-        validState = isValid;
-        extending = _extending;
-    }
-
+    /**
+     * Sets the piston update order by first removing the piston head.
+     *
+     * @param worldIn   current player world
+     * @param state     block state of the piston being activated
+     * @param pos       location of the piston being activated
+     * @param extending extending or retracting piston
+     */
     public static void setPistonMovement(World worldIn, IBlockState state, BlockPos pos, boolean extending) {
         EnumFacing enumfacing = (EnumFacing) state.getValue(BlockDirectional.FACING);
         IBlockState state1 = worldIn.getBlockState(pos.offset(enumfacing));
@@ -61,6 +61,28 @@ public class PistonHelper {
         }
     }
 
+    /**
+     * Set logic of the piston data.
+     *
+     * @param posIn
+     * @param btm
+     * @param btb
+     * @param isValid
+     * @param _extending
+     */
+    private static void set(BlockPos posIn, BlockPos[] btm, BlockPos[] btb, boolean isValid, boolean _extending) {
+        pistonPos = posIn;
+        tomove = btm;
+        tobreak = btb;
+        validState = isValid;
+        extending = _extending;
+    }
+
+    /**
+     * Draws the piston update order and other details.
+     *
+     * @param partialTicks tick sense last render
+     */
     public static void draw(float partialTicks) {
         if (Config.pistonVisualizer && activated) {
             final EntityPlayerSP player = Minecraft.getMinecraft().player;
@@ -71,14 +93,14 @@ public class PistonHelper {
             BlockPos pos;
 
             int count = 0;
-                for (int i = 1; i <= tobreak.length; i++) {
-                    pos = tobreak[tobreak.length - i];
-                    if (pos != null) {
-                        count++;
-                        EntityRenderer.drawNameplate(Minecraft.getMinecraft().fontRenderer, "\u00a7c" + count, (float) (pos.getX() + 0.5f - d0), (float) (pos.getY() + 0.5f - d1), (float) (pos.getZ() + 0.5f - d2), 0, rm.playerViewY, rm.playerViewX, rm.options.thirdPersonView == 2, false);
-                    }
-
+            for (int i = 1; i <= tobreak.length; i++) {
+                pos = tobreak[tobreak.length - i];
+                if (pos != null) {
+                    count++;
+                    EntityRenderer.drawNameplate(Minecraft.getMinecraft().fontRenderer, "\u00a7c" + count, (float) (pos.getX() + 0.5f - d0), (float) (pos.getY() + 0.5f - d1), (float) (pos.getZ() + 0.5f - d2), 0, rm.playerViewY, rm.playerViewX, rm.options.thirdPersonView == 2, false);
                 }
+
+            }
             int moved = -count;
             for (int i = 1; i <= tomove.length; i++) {
                 pos = tomove[tomove.length - i];
@@ -102,7 +124,7 @@ public class PistonHelper {
                 } else {
                     EntityRenderer.drawNameplate(Minecraft.getMinecraft().fontRenderer, pull, (float) (pos.getX() + 0.5f - d0), (float) (pos.getY() + 0.8f - d1), (float) (pos.getZ() + 0.5f - d2), 0, rm.playerViewY, rm.playerViewX, rm.options.thirdPersonView == 2, false);
                 }
-                EntityRenderer.drawNameplate(Minecraft.getMinecraft().fontRenderer, red + "===", (float) (pos.getX() + 0.5f - d0), (float) (pos.getY() + 0.2f - d1), (float) (pos.getZ() + 0.5f - d2), 0, rm.playerViewY, rm.playerViewX, rm.options.thirdPersonView == 2, false);
+                EntityRenderer.drawNameplate(Minecraft.getMinecraft().fontRenderer, red, (float) (pos.getX() + 0.5f - d0), (float) (pos.getY() + 0.2f - d1), (float) (pos.getZ() + 0.5f - d2), 0, rm.playerViewY, rm.playerViewX, rm.options.thirdPersonView == 2, false);
             }
             EntityRenderer.drawNameplate(Minecraft.getMinecraft().fontRenderer, gold + (moved < 0 ? 0 : moved), (float) (pos.getX() + 0.5f - d0), (float) (pos.getY() + 0.5f - d1), (float) (pos.getZ() + 0.5f - d2), 0, rm.playerViewY, rm.playerViewX, rm.options.thirdPersonView == 2, false);
         }
