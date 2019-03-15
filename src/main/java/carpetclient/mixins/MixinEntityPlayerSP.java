b819fe0e4183d2437a920b7e5419f09a18e3ab7c
Unknown <sahandhesar@gmail.com>
2019-03-15 19:41:20 +0100

    Elytra clipping fixes

--- src/main/java/carpetclient/mixins/MixinEntityPlayerSP.java
+++ src/main/java/carpetclient/mixins/MixinEntityPlayerSP.java
@@ -1,5 +1,6 @@
 package carpetclient.mixins;
 
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +12,7 @@ import carpetclient.Config;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = EntityPlayerSP.class, priority = 999)
 public class MixinEntityPlayerSP extends AbstractClientPlayer {
@@ -28,8 +30,22 @@ public class MixinEntityPlayerSP extends AbstractClientPlayer {
             setFlag(7, true);
     }
 
+    /**
+     * Clientside fix for player clipping through other players.
+     * @return
+     */
     public boolean canBePushed()
     {
         return Config.playerCollisions;
+    }
+
+    /**
+     * Fixes when going into elytra and cliping into roofs.
+     * @param pos
+     * @param cir
+     */
+    @Inject(method = "isOpenBlockSpace", at = @At("HEAD"), cancellable = true)
+    private void adjustIsOpenBlockSpace(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(!this.world.getBlockState(pos).isNormalCube());
     }
 }
