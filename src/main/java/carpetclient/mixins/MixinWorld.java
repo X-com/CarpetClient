763f944446f4c2b7ab5713d6d06afc0a500dd948
Unknown <sahandhesar@gmail.com>
2019-05-03 19:59:09 +0200

    Tile entity lag fix request by Red

--- src/main/java/carpetclient/mixins/MixinWorld.java
+++ src/main/java/carpetclient/mixins/MixinWorld.java
@@ -1,22 +1,32 @@
 package carpetclient.mixins;
 
 import carpetclient.Config;
-import carpetclient.rules.TickRate;
 import net.minecraft.block.Block;
-import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 @Mixin(World.class)
 public class MixinWorld {
+
+    @Shadow @Final public List<TileEntity> loadedTileEntityList;
+    @Shadow @Final public List<TileEntity> tickableTileEntities;
+    @Shadow @Final private List<TileEntity> tileEntitiesToBeRemoved;
 
     /**
      * Ignoring entity's when placing  block clientside.
@@ -37,4 +47,21 @@ public class MixinWorld {
     ){
         return Config.ignoreEntityWhenPlacing || world.checkNoEntityCollision(bb, entityIn);
     }
+
+    /**
+     * Tile entity removal lag fix from forge.
+     *
+     * @param ci
+     */
+    @Inject(method = "updateEntities", at = @At(value = "FIELD", target = "Lnet/minecraft/world/World;tileEntitiesToBeRemoved:Ljava/util/List;", ordinal = 0))
+    private void tileEntityRemovalFix(CallbackInfo ci){
+        if (!this.tileEntitiesToBeRemoved.isEmpty()) {
+            Set<TileEntity> remove = Collections.newSetFromMap(new java.util.IdentityHashMap<>());
+            remove.addAll(this.tileEntitiesToBeRemoved);
+            this.tickableTileEntities.removeAll(remove);
+            this.loadedTileEntityList.removeAll(remove);
+            this.tileEntitiesToBeRemoved.clear();
+        }
+    }
+
 }
