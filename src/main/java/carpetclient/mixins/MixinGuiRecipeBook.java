d975589db47debd10d56d094d4629aefba03ce86
Unknown <sahandhesar@gmail.com>
2020-05-26 14:18:18 +0200

    Added middle click  to set search field

--- src/main/java/carpetclient/mixins/MixinGuiRecipeBook.java
+++ src/main/java/carpetclient/mixins/MixinGuiRecipeBook.java
@@ -2,7 +2,10 @@ package carpetclient.mixins;
 
 import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.gui.recipebook.GuiRecipeBook;
+import net.minecraft.client.gui.recipebook.RecipeBookPage;
 import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.crafting.IRecipe;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,6 +22,16 @@ public class MixinGuiRecipeBook {
 
     @Shadow public boolean isVisible(){return false;}
 
+    @Shadow @Final private RecipeBookPage recipeBookPage;
+
+    @Shadow private int width;
+
+    @Shadow private int height;
+
+    @Shadow private int xOffset;
+
+    @Shadow private void updateCollections(boolean p_193003_1_) {}
+
     private static String memoText = "";
 
     @Inject(method = "initVisuals", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/recipebook/GuiRecipeBook;searchBar:Lnet/minecraft/client/gui/GuiTextField;", ordinal = 4, shift = At.Shift.AFTER))
@@ -32,5 +45,20 @@ public class MixinGuiRecipeBook {
     @Inject(method = "keyPressed", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/recipebook/GuiRecipeBook;lastSearch:Ljava/lang/String;", ordinal = 1, shift = At.Shift.AFTER))
     private void setSearchText(char typedChar, int keycode, CallbackInfoReturnable<Boolean> c) {
         memoText = lastSearch;
+    }
+
+    @Inject(method = "mouseClicked", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/recipebook/GuiRecipeBook;recipeBookPage:Lnet/minecraft/client/gui/recipebook/RecipeBookPage;", ordinal = 2, shift = At.Shift.AFTER))
+    private void setSearchText(int p_191862_1_, int p_191862_2_, int p_191862_3_, CallbackInfoReturnable<Boolean> c) {
+        if(p_191862_3_ == 2) {
+            try {
+                recipeBookPage.mouseClicked(p_191862_1_, p_191862_2_, 0, (this.width - 147) / 2 - this.xOffset, (this.height - 166) / 2, 147, 166);
+                IRecipe irecipe = recipeBookPage.getLastClickedRecipe();
+                String s = irecipe.getRecipeOutput().getDisplayName();
+                searchBar.setText(s);
+                lastSearch = s;
+                memoText = s;
+                this.updateCollections(true);
+            }catch (Exception e){}
+        }
     }
 }
